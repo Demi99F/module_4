c93f379564d4556fdaba10020ee9b9522c541b3b
Demi99F <101418339+Demi99F@users.noreply.github.com>
2022-12-06 13:09:19 +0100

    Add files via upload

---
+++ piano_opdracht/assets/js/script.js
@@ -0,0 +1,40 @@
+document.getElementById("c").onclick = function(){ // als op de c-toets wordt geklikt
+	var c_toets = document.getElementById("audio_c"); // zoek het c-toets audio-element op
+	c_toets.play(); // speel het audio-element af
+}
+document.getElementById("d").onclick = function(){
+	var d_toets = document.getElementById("audio_d");
+	d_toets.play();
+}
+document.getElementById("e").onclick = function(){
+	var e_toets = document.getElementById("audio_e");
+	e_toets.play();
+}
+document.getElementById("f").onclick = function(){
+	var f_toets = document.getElementById("audio_f");
+	f_toets.play();
+}
+document.getElementById("g").onclick = function(){
+	var g_toets = document.getElementById("audio_g");
+	g_toets.play();
+}
+document.getElementById("a").onclick = function(){
+	var a_toets = document.getElementById("audio_a");
+	a_toets.play();
+}
+document.getElementById("b").onclick = function(){
+	var b_toets = document.getElementById("audio_b");
+	b_toets.play();
+}
+document.getElementById("c1").onclick = function(){
+	var c1_toets = document.getElementById("audio_c1");
+	c1_toets.play();
+}
+document.getElementById("d1").onclick = function(){
+	var d1_toets = document.getElementById("audio_d1");
+	d1_toets.play();
+}
+document.getElementById("e1").onclick = function(){
+	var e1_toets = document.getElementById("audio_e1");
+	e1_toets.play();
+}
